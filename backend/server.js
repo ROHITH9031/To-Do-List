6df7b847ca0db95c9dd965d48b789cf9e66b0512
AtyamRohith <atyamrohith2005@gmail.com>
2026-09-01 21:43:56 +0530

    Configure production CORS

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,13 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://to-do-list-sigma-lilac.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Test route
